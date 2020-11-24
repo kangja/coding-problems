21514b65a67ba200d25bb3710142f68dc2007f4a
hi <kangja1996@gmail.com>
2020-11-24 15:45:33 -0500

    progress

--- exercise-52-interview-question.js
+++ exercise-52-interview-question.js
@@ -42,22 +42,67 @@
 
 // Brute force approach = an easy solution which is not the most efficient but usually is the easiest one or the first one that comes up in mind. 
 
-// So in our case, if we look at this during an interview with a time limit, you might say this looks like a nested loop where we're comparing a with z, a with y, then a with i. And then we go b with z, by with y, b with i and etc. So immediately this looks like we're comparing 2 different arrays and we're going to have 2 for loops. One nested on top of each other and right away, we know thae 
-const commonItems = (array1, array2) => {
-  for (let i = 0; i < array1.length; i++) {
-    for (let j = 0; j < array2.length; j++) {
-      if (array1[i] == array2[j]) {
-        return true
-      }
-      else {
-        return false 
-      }
-    }
-  }
+// So in our case, if we look at this during an interview with a time limit, you might say this looks like a nested loop where we're comparing a with z, a with y, then a with i. And then we go b with z, by with y, b with i and etc. So immediately this looks like we're comparing 2 different arrays and we're going to have 2 for loops. One nested on top of each other and right away, we know that's a Big O of O(n^2). 
+
+// Most likely, during an interview, nested for loops is something that we want to avoid. Just telling the interviewer this solution even though it's not the best solution already shows that you're thinking clearly about the problem. And it also gives you a point from where to improve from now. You don't necesssarily have to code this part and it's usually enough just to describe how it works. But depending on your time, you may or may not want to code this as well. 
+
+// The important thing is that you let the interviewer know that this is the brute force or the easy naive solution that may not be the most efficient. At least this way, yo have the right answer, then running out of time and not even having an answer. 
+
+// #6. Tell why this approach is not the best. In our case, we said that the code might not be efficient or we might not have a readable code or code might be really complicated and hard to read for other developers. 
+
+// solution for brute force to show why it is O(n^2). I'm going to just code and most likely you wonldn't do this in an interview. 
+
+// const array1 = ["a", "b", "c", "x"];
+// const array2 = ["z", "y", "a"];
+
+// function containsCommonItem(arr1, arr2,) {
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr2.length; j++) {
+//       if (arr1[i] === arr2[j]) {
+//         return true
+//       }
+//     }
+//   }
+//   return false 
+// }
+
+// console.log(containsCommonItem(array1, array2))
+// we have 2 for loops. 1 nested after another. When it comes to time complexity here, it is O(a*b) because we have 2 different arrays(array 1 and array2). These 2 arrays can be different sizes. We can't just say O(n^2). Instead of O(n^2), it's more like O(a*b). The time will be slow. 
+
+// #7. One of the most common patterns when it comes to nested for loops is that you might be able to use something called Hash Tables. In JavaScript, they're called objects in order to speed things up. 
+
+// Is there a way to turn this O(a*b), the time complexitiy, into something faster? Because we're doing a bit of unncessary work where we are just keep looping a z, a y, a a and then we go b z, b y, b a and so on. We're doing this comparision in the same work over and over. 
+
+// What if we have a better solution?
+
+
+// const array1 = ["a", "b", "c", "x"];
+// const array2 = ["z", "y", "a"];
+
+// What if we had a function that takes the 1st array? Array 1 is going to be converted to an obejct.
+// array 1 ==> obj {
+// a: true,
+// b: true,
+// c: true, 
+// x: true
+// }
+
+// can we check each item in array 2 to this object and see if a property exists? Ideally, we can do something like 
+// array2[index] === obj.properties.
+// ^ this is a commpn pattern you will see alot when it comes to improving time complexity.  
+
+// If I am able to convert the first array into an object, now, instead of having to loop over both arrays, I can just loop through the second array and just simply check if the property "z" exist in the object that we created. No it doesn't. Then, does the property "y" exist on this array? No it doesn't. Does a property "a" exist on this array? Yes it does. Then, it means we have a common item.
+
+// 8. We're going to create a new function and this function is going to say, 
+function containsCommonItem2(arr1, arr2) {
+  // I want you to loop through first array and create object where properties equal items in the array.
+  // The second thing will be to loop through second array and check if item in second array exists on created object. 
+  // Now before we even start coding this, I can right away sense that we're going to have a better performance that the first solution that was O(a*b) because we're going to have 2 loops, but these 2 loops AREN'T going to be nested. 
+
+  // Remember in the first solution, we had nested for loops. In the second solution, we will have 2 separate for loops. And if you remember our Big O analysis, when we have steps one after another, instead of nested, the big o becomes a + b. We have a potential solution here and have a time complexity of a + b instead of a * b. Thoese are big savings. 
+
+  // Although we haven't given a clear solution, we've been talking about how we solve the problem. The interviewer sees how we think, sees what steps we take, sees that were making smart critical decisions to go from understanding the problem to solving the problem in a naive way. Understanding why this might not be the best way and then start to think about possible solutions. 
+
+  // The beauty of this approach is that no matter whether we get this part correct and maybe our assumption is wrong, we are showing the interviewer that were thinking through a problem and we're making small logical steps towards a solution. And this is a lot better than just coding right away trying a solution and to if you get it right or wrong. An interviewer is going to have a good understanding of your abilities and your thought process this way. And these are tye type of people that they want to hire. 
+
 }
-
-const array1 = ["a", "b", "c", "x"];
-const array2 = ["z", "y", "x"];
-
-console.log(commonItems(array1, array2))
-
